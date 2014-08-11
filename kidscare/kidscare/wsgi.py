a1595f7ccb6ebc9be7ebb916d39a4ea3c6922c09
root <root@iZ23otdlkscZ.(none)>
2014-08-11 15:16:16 +0800

    adapt the wsgi to uwsgi and nginx

--- kidscare/kidscare/wsgi.py
+++ kidscare/kidscare/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/root/kidscare_src/KidsCare_Web/kidscare')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kidscare.settings")
 
 from django.core.wsgi import get_wsgi_application
