ea2df8ec97571113bbe3eb622c00219fdca716f8
AutoCoder <tj_liyuan@163.com>
2015-02-13 15:56:04 +0800

    remove v-toy test code

--- kidscare/kidscare/urls.py
+++ kidscare/kidscare/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from settings import STATICFILES_DIRS
-from views import hello, rtthreadtext, rtthreadaudio, rtthreadaudiostream
+from views import hellomilk
 from mombabyprods.views import handleWXHttpRequest
 
 admin.autodiscover()
@@ -11,9 +11,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^milk/', include('kidscare.mombabyprods.urls')),
     url(r'^$', handleWXHttpRequest), # for weixin handler
-    url(r'^hello/$', hello),
-	url(r'^rtthreadtext/$', rtthreadtext),
-    url(r'^rtthreadaudio/$',rtthreadaudio),
-    url(r'^rtthreadaudiostream/$',rtthreadaudiostream),
+    url(r'^hellomilk/$', hellomilk),
     url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':str(STATICFILES_DIRS[0]), 'show_indexes': True}),
 )
